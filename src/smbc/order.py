7aefa116e1dc9e1ad0fe99dc0d2982b34429c474
POI <67753238+poi-ai@users.noreply.github.com>
2024-03-25 01:08:23 +0900

    SMBC在庫自動取得処理作成(エラーリカバリ以外)

--- src/smbc/order.py
+++ src/smbc/order.py
@@ -1,3 +1,5 @@
+import config
+from datetime import datetime, timedelta
 from bs4 import BeautifulSoup
 
 class Order():
@@ -16,7 +18,7 @@ class Order():
         一般信用売り注文確認画面へリクエストを送る
 
         Args:
-            session(): ログイン状態のセッション
+            session(requests.sessions.Session): ログイン状態のセッション
             stock_code(str): 対象銘柄
             num(int): 発注数量
 
@@ -26,33 +28,35 @@ class Order():
 
         '''
         post_info = {
-            'odrJoken': '1',
+            'odrJoken': '1',               # 1: 通常注文、2: 逆指値注文
             'execCnd': '0',
             'ippanSinyoTriKanoFlg': '1',
             'kakunin': '1',
-            'odrExecYmd': '',
+            #'odrExecYmd': '',
             'dispChk': '0',
-            'honjituIjiRitu': '--.--',
-            'shintategaku': '1,000,000',
-            'specifyMeig': '1',
-            'meigCd': stock_code, # 証券コード
+            #'honjituIjiRitu': '100.00',   # 維持率(%)
+            #'shintategaku': '1,000,000',  # 信用建可能額
+            'specifyMeig': '',
+            'meigCd': stock_code,          # 証券コード
             'sinkbShitei': '0',
             'sijyoKbn': '1',
-            'sinyoToriKbn': '1',
-            'suryo': num,
-            'kakaku': '',
-            'nariSasiKbn': '2',
-            'cnd17': '0',
-            'yukokikan': '1',
-            'yukokigenDate': '',
-            'kozaKbnSinyo': '1',
+            'sinyoToriKbn': '1',           # 0: 制度、1: 信用
+            'suryo': num,                  # 株数
+            #'kakaku': '',                 # 指値注文価格
+            'nariSasiKbn': '2',            # 1: 指値、2: 成行
+            #'cnd11': '2',                 # 寄り成り
+            #'cnd12': '3',                 # 引け成り
+            'cnd17': '0',                  # 条件なし成行
+            'yukokikan': '1',              # 1: 当日中、9: 期間指定
+            'yukokigenDate': '',           # 注文有効期間
+            'kozaKbnSinyo': '1',           # 0: 一般口座、1: 特定口座
             'kozaKyakKbn': '1',
-            'execUrl.x': '0',
-            'execUrl.y': '0'
+            'execUrl.x': '99',             # 押下したボタンのx軸
+            'execUrl.y': '18'              # 押下したボタンのy軸
         }
 
         try:
-            r = session.post('https://trade.smbcnikko.co.jp/OdrMng/21ABM0679964/sinyo/tku_odr/siji', json = post_info)
+            r = session.post('https://trade.smbcnikko.co.jp/OdrMng/000000000000/sinyo/tku_odr/siji', data = post_info)
         except:
             self.log.error('接続に失敗')
             return False, None
@@ -70,6 +74,106 @@ class Order():
 
         return True, soup
 
-    def order(self, session, stock_code):
-        '''注文を行う'''
-        pass
+    def order(self, session, stock_code, num, token_id, url_id):
+        '''
+        一般信用売り注文リクエストを送る
+
+        Args:
+            session(requests.sessions.Session): ログイン状態のセッション
+            stock_code(str): 対象銘柄
+            num(int): 発注数量
+            token_id(str): トークンID(注文確認画面で発行)
+            url_id(str): URL ID(注文確認画面で発行)
+
+        Returns:
+            result(bool): 実行結果
+            soup(bs4.BeautifulSoup): レスポンスのHTML
+
+        '''
+        post_info = {
+            'specifyMeig': '1',
+            'sinkbShitei': '0',
+            'meigCd': stock_code,                      # 証券コード
+            'bbaiKbn': '1',
+            'sijyoKbn': '1',
+            'execCnd': '0',
+            'nariSasiKbn': '2',
+            'kakaku': '',
+            'suryo': num,
+            'odrExecYmd': self.next_weekday(),         # 注文日
+            'expcheck': '0',
+            'yukoSiteDate': '1',
+            'yukokigenDate': '',                       # 注文有効期間
+            'nyuKeroKbn': '',
+            'execCndKbn': '',
+            'kanriKesaiKbn': '',
+            'jreit': '',
+            'etf': '',
+            'kozaKbnSinyo': '1',                       # 0: 一般口座、1: 特定口座
+            'kozaKyakKbn': '1',
+            'tokenId': token_id,                       # 注文確認画面で取得したトークンID
+            'toriPasswd': config.SMBC_TRADE_PASSWORD,  # 取引パスワード
+            'funcId': '01',
+            'x': '0',
+            'y': '0',
+            'shintategaku': '1,000,000',
+            'odrJoken': '1',                           # 1: 通常注文、2: 逆指値注文
+            'cnd11': '',
+            'cnd12': '',
+            'cnd13': '',
+            'cnd14': '',
+            'cnd15': '',
+            'cnd16': '',
+            'cnd17': 'checked',
+            'dosokuKehai': '',
+            'oya': '',
+            'sinyoToriKbn': '1',                       # 0: 制度、1: 信用
+            'ippanSinyoTriKanoFlg': '1',
+            'trgKbn': '',
+            'trgKBkakaku': '',
+            'trgKbnHZ': '',
+            'trgKbnPM': '',
+            'trgHZkakaku': '',
+            'trgKbnJouge': '',
+            'nyuKeroKbn2': 'pcw00'
+        }
+
+        try:
+            r = session.post(f'https://trade.smbcnikko.co.jp/OdrMng/{url_id}/sinyo/tku_odr/exec', data = post_info)
+        except:
+            self.log.error('接続に失敗')
+            return False, None
+
+        if r.status_code != 200:
+            self.log.error(f'接続に失敗 ステータスコード: {r.status_code}')
+            return False, None
+
+        soup = BeautifulSoup(r.content, 'lxml')
+
+        # セッション切れエラー
+        if 'NOL11007E' in soup.text:
+            self.log.error('セッション切れエラー')
+            return False, None
+
+        return True, soup
+
+    def next_weekday(self):
+        '''
+        次の営業日を返す(祝日非対応)
+
+        Returns:
+            date(str): 次の営業日(yyyymmdd)
+
+        '''
+        now = datetime.now()
+        # 土曜
+        if now.weekday == 4:
+            next_weekday = now + timedelta(days = 3)
+        # 金曜
+        elif now.weekday == 5:
+            next_weekday = now + timedelta(days = 2)
+        # 平日/日曜
+        else:
+            next_weekday = now + timedelta(days = 1)
+
+        return next_weekday.strftime("%Y%m%d")
--- src/steal.py
+++ src/steal.py
@@ -1,5 +1,6 @@
 import csv
 import time
+import re
 from main import Main
 from datetime import datetime
 
@@ -32,17 +33,57 @@ class Steal(Main):
             return False
         self.log.info('SMBC日興証券ログイン終了')
 
+
+        # 在庫チェック/注文処理
+        #while True:
+
+        # 除外銘柄管理用のリスト作成
+        tmp_list = steal_list
+
         # 時間再チェック
         result = self.time_manage()
         if result == False:
             self.log.info('取引時間中なので監視/注文処理は行いません')
             exit()
 
-        # 在庫チェック/注文処理
-        while True:
-            for target in steal_list:
-                # 在庫チェック/注文発注
-                self.order_exec(target)
+        for target in steal_list:
+            # セッションが切れている場合は再ログイン
+            if self.smbc_session == False:
+                self.log.info('SMBC日興証券再ログイン開始')
+                self.smbc_session = self.smbc.login.login()
+                if self.smbc_session == False:
+                    return False
+                self.log.info('SMBC日興証券再ログイン終了')
+
+            # 在庫チェック/注文発注
+            self.log.info(f'在庫チェック/注文を行います 証券コード: {target[0]}, 株数: {target[1]}')
+            result = self.order_exec(target)
+            self.log.info(f'在庫チェック/注文処理終了 証券コード: {target[0]}, 株数: {target[1]}')
+
+            # 結果チェック
+            # 続行不可能なエラー
+            if result == 'mistake password':
+                self.log.error('パスワードが誤っているため処理を終了します')
+                exit()
+
+            # 再ログインした方がいいエラー(接続エラー/不明なエラー)
+            elif result == 'confirm connect' or 'order connect' or 'confirm unknown' or 'order unknown':
+                self.smbc_session == False
+
+            # チェックした銘柄を監視対象から外す(余力不足/非取扱銘柄/注文成功)
+            elif result == 'lacking money' or 'not handle' or 'success':
+
+                # 処理対象リストから外す
+                tmp_list = [sublist for sublist in tmp_list if sublist[0] != 20]
+
+
+            # TODO メンテナンス中の処遇
+
+            if self.limiter:
+                time.sleep(2)
+
+        # 除外した銘柄情報をメインリストに反映
+        steal_list = tmp_list
 
     def order_exec(self, target):
         '''
@@ -53,26 +94,66 @@ class Steal(Main):
                 ※銘柄コード、発注数、備考
 
         Returns:
-            result(bool): 実行結果
+            error_reason(str): 処理失敗原因
 
         '''
         stock_code = target[0]
         num = target[1]
 
         # 一般売注文確認画面へリクエストを送る
-        result, html = self.smbc.order.confirm(target, num)
+        result, soup = self.smbc.order.confirm(self.smbc_session, stock_code, num)
         if result == False:
-            self.smbc_session = False
+            return 'confirm connect'
 
-        # レスポンスチェック、在庫があれば正常の確認画面へ遷移するはず
+        # メンテナンスチェック
+        # 時間制御しているから定期メンテではここは引っかからない
+        if 'NOL11001E' in soup.text:
+            self.log.warning('メンテナンス中のため注文できません')
+            return 'maintenance'
 
-        # TODO 注文パラメータおかしいよチェック
+        # 取扱チェック
+        if 'NOL51305E' in soup.text:
+            self.log.warning('一般信用非取扱銘柄のため注文できません')
+            return 'not handle'
 
-        # TODO 在庫足りんよチェック
+        # 余力チェック
+        if 'NOL51015E' in soup.text:
+            self.log.warning('余力が足りないため注文できません')
+            return 'lacking money'
 
-        # TODO 余力足りんよチェック
+        # 在庫チェック
+        if 'NOL75401E' or 'NOL75400E' in soup.text:
+            self.log.info('一般信用在庫が足りないため注文できません')
+            return 'lacking stock'
 
-        # TODO 注文リクエスト
+        # 注文用のトークンID/URLIDの取得
+        try:
+            token_id = soup.find('input', {'name': 'tokenId'}).get('value')
+            url_match = re.search(r'OdrMng/(.+)/sinyo/tku_odr/exec', str(soup))
+            url_id = url_match.groups()[0]
+        except Exception as e:
+            #print(soup)
+            self.log.error(f'不明なエラーです\n{e}')
+            return 'confirm unknown'
+
+        # 注文リクエストを送る
+        result, soup = self.smbc.order.order(self.smbc_session, stock_code, num, token_id, url_id)
+        if result == False:
+            return 'order connect'
+
+        # パスワードチェック
+        if 'NOL76511E' in soup.text:
+            self.log.error('パスワードが正しくありません')
+            return 'mistake password'
+
+        # 注文完了チェック
+        if not '売り注文を受付ました' in soup.text:
+            self.log.error('不明なエラーです')
+            return 'order unknown'
+
+        self.log.info(f'注文が完了しました 証券コード: {stock_code}')
+
+        return 'success'
 
 
     def time_manage(self):
@@ -80,12 +161,18 @@ class Steal(Main):
         # 時間取得
         now = datetime.now()
 
+        ## 全日共通時間判定
+
         # メンテナンス時間(2:00~4:59)なら5:00まで待つ
         if 2 <= now.hour <= 4:
             target_hour, target_minute = 5, 0
 
+        ## 営業日の時間判定
+        if now.weekday == 5 or 6:
+            return True
+
         # 大引け後注文中断時間(15:00~16:58)なら16:59まで待つ
-        elif now.hour == 15 or (now.hour == 16 and now.minute < 59):
+        if now.hour == 15 or (now.hour == 16 and now.minute < 59):
             target_hour, target_minute = 15, 59
 
         # 16:59なら17:00まで待つ
@@ -121,7 +208,7 @@ class Steal(Main):
         steal_list = []
 
         try:
-            with open('steal_list.csv', 'r', newline='') as csvfile:
+            with open('steal_list.csv', 'r', newline = '', encoding = 'UTF-8') as csvfile:
                 reader = csv.reader(csvfile)
                 for row in reader:
                     steal_list.append(row)
